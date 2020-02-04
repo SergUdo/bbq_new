b62ef653135cc656bfd949d4a2b81f95cf471c42
SergUdo <sergii.udovichenko@gmail.com>
2020-02-04 16:30:24 +0200

    Add migration

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,7 @@
 class Event < ActiveRecord::Base
-  # Валидируем заголовок, он не может быть длиннее 255 букв
-  validates :title, presence: true, length: {maximum: 255}
-  # У события должны быть заполнены место и время
-  validates :address, presence: true
-  validates :datetime, presence: true
-end
+  # Событие принадлежит юзеру
+  belongs_to :user
 
+  # Валидируем юзера на присутствие. В Rails 5 связи
+  # валидируются по умолчанию
+  validates :user, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_04_125137) do
+ActiveRecord::Schema.define(version: 2020_02_04_141854) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,16 @@ ActiveRecord::Schema.define(version: 2020_02_04_125137) do
     t.datetime "datetime"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_events_on_user_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "events", "users"
 end
