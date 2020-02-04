d66cb9897b39814cae28e1bff53282245f3dae8e
SergUdo <sergii.udovichenko@gmail.com>
2020-02-04 16:43:30 +0200

    Add bootstrap

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,5 @@ class Event < ActiveRecord::Base
   # Событие принадлежит юзеру
   belongs_to :user
 
-  # Валидируем юзера на присутствие. В Rails 5 связи
-  # валидируются по умолчанию
   validates :user, presence: true
+end
