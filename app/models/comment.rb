bec99f1ecbd1512a2cd8e6da8e219245c7d8f572
SergUdo <sergii.udovichenko@gmail.com>
2020-02-10 19:17:23 +0200

    Add validates

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,10 @@
 class Comment < ApplicationRecord
   belongs_to :event
   belongs_to :user
+
+  has_many :comments, dependent: :destroy
+
+  validates  :event, presence: true
+  validates :body, presence: true
+  validates :user_name, presence: true, unless: -> { user.present? }
 end
