cbdf0399732833f212936bce456259089928059b
Sergii Udovichenko <sergii.udovichenko@gmail.com>
2020-05-04 01:03:26 +0300

    Update

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,8 +8,8 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
-require("ekko-lightbox/ekko-lightbox")
-require("imagesloaded/imagesloaded")
+// require("ekko-lightbox/ekko-lightbox")
+// require("imagesloaded/imagesloaded")
 
 import "cloudinary"
 import "bootstrap"
