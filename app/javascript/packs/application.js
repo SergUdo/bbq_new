8fa9f61ca599823478575018bd4578daeb8d0e62
Sergii Udovichenko <sergii.udovichenko@gmail.com>
2020-05-04 00:22:36 +0300

    Update package.json

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,7 +12,7 @@ require("jquery")
 require("ekko-lightbox/ekko-lightbox")
 require("imagesloaded/imagesloaded")
 
-import ("cloudinary")
+import("cloudinary")
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
