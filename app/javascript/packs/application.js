f8c89e451bd84bdd5c571684fa8ad34dddd0d3af
Sergii Udovichenko <sergii.udovichenko@gmail.com>
2020-05-06 20:25:45 +0300

    Added script in application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,6 +13,7 @@ require("jquery")
 
 import "cloudinary"
 import "bootstrap"
+import "ekko-lightbox/dist/ekko-lightbox"
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -26,3 +27,8 @@ import "bootstrap"
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+$(document).on('click', '[data-toggle="lightbox"]', function(event) {
+                event.preventDefault();
+                $(this).ekkoLightbox();
+            });
