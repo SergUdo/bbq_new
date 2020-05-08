014806c58816531a909e4474d278a9347d013fe2
Sergii Udovichenko <sergii.udovichenko@gmail.com>
2020-05-09 00:25:41 +0300

    Added settings cloudinary

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,14 +8,14 @@ require("@rails/activestorage").start()
 require("channels")
 require("jquery")
 require("bootstrap/dist/js/bootstrap")
+//require ("cloudinary")
+// require("imagesloaded/imagesloaded")
 
 import "cloudinary"
 import "../stylesheets/application"
 import "ekko-lightbox/dist/ekko-lightbox"
 //require("turbolinks").start()
 //import "bootstrap"
-// import "ekko-lightbox/ekko-lightbox"
-// require("imagesloaded/imagesloaded")
 // import "ekko-lightbox/ekko-lightbox"
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
