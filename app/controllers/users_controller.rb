6c405d33a0ba01ef1ff891f6c71b02fd6938df56
Sergii Udovichenko <sergii.udovichenko@gmail.com>
2020-05-08 17:54:15 +0300

    Added script

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,10 +18,6 @@ class UsersController < ApplicationController
 
   private
 
-  # def set_user
-  #   @user = User.find(params[:id])
-  # end
-
   def user_params
     params.require(:user).permit(:name, :email, :avatar)
   end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,12 +8,11 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
-// require("ekko-lightbox/ekko-lightbox")
-// require("imagesloaded/imagesloaded")
 
 import "cloudinary"
 import "bootstrap"
-import "ekko-lightbox/dist/ekko-lightbox"
+// require("imagesloaded/imagesloaded")
+// import "ekko-lightbox/ekko-lightbox"
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
