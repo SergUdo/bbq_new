a35bad8f2530c153ade211b393268482d224af68
Sergii Udovichenko <sergii.udovichenko@gmail.com>
2020-05-10 17:42:41 +0300

    Changed javascript files

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,14 +9,14 @@ require("channels")
 require("jquery")
 require("bootstrap/dist/js/bootstrap")
 //require ("cloudinary")
-// require("imagesloaded/imagesloaded")
+//require("imagesloaded/imagesloaded")
 
 import "cloudinary"
 import "../stylesheets/application"
 import "ekko-lightbox/dist/ekko-lightbox"
 //require("turbolinks").start()
 //import "bootstrap"
-// import "ekko-lightbox/ekko-lightbox"
+//import "ekko-lightbox/ekko-lightbox"
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
